ddbc95fe73823a6acced6d6b25bcb790ac6ea0ef
AppoGit-Hub <bl@dtd.be>
2023-05-01 10:32:35 +0200

    extra Utensil Stack

--- src/interfaceAccess/UtensilDataAccess.java
+++ src/interfaceAccess/UtensilDataAccess.java
@@ -1,8 +1,12 @@
 package interfaceAccess;
 
+import exception.AllUtensilException;
 import exception.UtensilException;
 import model.Utensil;
 
+import java.util.ArrayList;
+
 public interface UtensilDataAccess {
     Utensil getUtensil(String name) throws UtensilException;
+    ArrayList<Utensil> getAllUtensil() throws AllUtensilException;
 }
