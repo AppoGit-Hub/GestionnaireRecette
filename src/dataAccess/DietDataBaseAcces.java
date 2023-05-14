3508498132cd11bc1f6bd8c1ea3a99e0360a1b23
AppoGit-Hub <bl@dtd.be>
2023-05-14 13:22:20 +0200

    added diet date base acces

--- src/dataAccess/DietDataBaseAcces.java
+++ src/dataAccess/DietDataBaseAcces.java
@@ -1,9 +1,14 @@
 package dataAccess;
 
 import exception.AllDietException;
+import exception.AllEquipementException;
 import interfaceAccess.DietDataAccess;
 import model.Diet;
+import model.Equipment;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -11,10 +16,21 @@ public class DietDataBaseAcces implements DietDataAccess {
     @Override
     public ArrayList<Diet> getAllDiet() throws AllDietException {
         try {
-            // delete this after
-            throw new SQLException();
+            Connection connexion = SingletonConnexion.getInstance();
+            String query = "SELECT * FROM diet;";
+            PreparedStatement statement = connexion.prepareStatement(query);
+            ResultSet data = statement.executeQuery();
+            ArrayList<Diet> diets = new ArrayList<Diet>();
+            while (data.next()) {
+                int id = data.getInt("id");
+                String name = data.getString("name");
+                Diet diet = new Diet(id, name);
+                diets.add(diet);
+            }
+            return diets;
         } catch (SQLException exception) {
             throw new AllDietException();
         }
+
     }
 }
