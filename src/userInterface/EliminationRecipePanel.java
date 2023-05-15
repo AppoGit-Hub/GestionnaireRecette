3b12b7b283771a662f6e3dfed0ac115a58b6fd64
AppoGit-Hub <bl@dtd.be>
2023-05-15 21:02:46 +0200

    fix merge extra and code quality

--- src/userInterface/EliminationRecipePanel.java
+++ src/userInterface/EliminationRecipePanel.java
@@ -11,13 +11,11 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import static javax.swing.JOptionPane.OK_OPTION;
+import static javax.swing.JOptionPane.YES_OPTION;
 
 public class EliminationRecipePanel extends JPanel implements ActionListener {
-    private ListingRecipeTableModel allRecipePanel;
-    private JScrollPane jScrollPane;
     private JTable jTable;
-    private JPanel jNorthBottonPanel;
-    private JButton deleteButton,addButton,changeButton;
+    private JButton deleteButton;
     private ListSelectionModel listSelectionModelRecipe;
     private RecipeController recipeController;
     private RecipeStepController recipeStepController;
@@ -26,82 +24,57 @@ public class EliminationRecipePanel extends JPanel implements ActionListener {
     private CommentController commentController;
     private PeriodController periodController;
     private OrderTypeController orderTypeController;
-    private ArrayList<Recipe> setRecipe;
-
     public EliminationRecipePanel(){
         this.setLayout(new BorderLayout());
-        deleteButton = new JButton("Elimination");
+
+        this.recipeStepController = new RecipeStepController();
+        this.lineRecipeController = new LineRecipeController();
+        this.equipementController = new EquipementController();
+        this.commentController = new CommentController();
+        this.periodController = new PeriodController();
+        this.orderTypeController = new OrderTypeController();
+        this.recipeController = new RecipeController();
+
+        this.deleteButton = new JButton("Delete");
         this.deleteButton.addActionListener(this);
 
+        this.jTable = new JTable(new ListingRecipeTableModel());
+        this.jTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        this.jTable.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);//je ne suis pas sûr de ce que cela veut dire
+        this.listSelectionModelRecipe = this.jTable.getSelectionModel( );
+
+        this.add(new JScrollPane(jTable), BorderLayout.CENTER);
         this.add(deleteButton, BorderLayout.NORTH);
-
-        this.allRecipePanel = new ListingRecipeTableModel();
-        this.jTable = new JTable(allRecipePanel);
-        jTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-        jTable.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);//je ne suis pas sûr de ce que cela veut dire
-        this.listSelectionModelRecipe = jTable.getSelectionModel( );
-        jScrollPane = new JScrollPane(jTable);
-        this.add(jScrollPane,BorderLayout.CENTER);
-
-        JLabel helpMessage = new JLabel("pour celectionner plusieur ligne maintener la touche ctrl enfoncé puis clique sur les éléments souhaités");
-        helpMessage.setHorizontalAlignment(SwingConstants.CENTER);
-        this.add(helpMessage,BorderLayout.SOUTH);
     }
-    public void actionPerformed (ActionEvent e){
-        String source = e.getActionCommand();
-        int indexRecipeSelection = this.listSelectionModelRecipe.getMinSelectionIndex();
-        if(indexRecipeSelection != -1){
-            try{
-                this.recipeStepController = new RecipeStepController();
-                this.lineRecipeController = new LineRecipeController();
-                this.equipementController = new EquipementController();
-                this.commentController = new CommentController();
-                this.periodController = new PeriodController();
-                this.orderTypeController = new OrderTypeController();
-                this.recipeController = new RecipeController();
-
-                this.setRecipe = recipeController.getAllRecipe();
+    public void actionPerformed(ActionEvent event) {
+        String source = event.getActionCommand();
+        if (source.equals("Delete")) {
+            try {
                 int confirmation = JOptionPane.showConfirmDialog(
                         null,
-                        "voulez vous vraiment éliminer ces/cette recette(s)",
-                        "confirmation : élimination : données lié à une recette déjà éliminé",
+                        "Do you really want to delete this recipe(s) ?",
+                        "This is permanent !",
                         JOptionPane.YES_NO_OPTION);
-                // todo : c'est quoi le parentComponent de JOption
-                while(confirmation == OK_OPTION && indexRecipeSelection <= this.listSelectionModelRecipe.getMaxSelectionIndex()) {
-                    if (listSelectionModelRecipe.isSelectedIndex(indexRecipeSelection)){
-                        System.out.println("index :"+indexRecipeSelection+"code recipe"+"codeRecipe"+"\n");
-                        int codeRecipe = setRecipe.get(indexRecipeSelection).getCode();
-                        recipeStepController.deleteAllStepRecipe(codeRecipe);
-                        lineRecipeController.deleteAllLineRecip(codeRecipe);
-                        equipementController.deleteAllEquipement(codeRecipe);
-                        commentController.deleteAllComment(codeRecipe);
-                        periodController.deleteAllPeriod(codeRecipe);
-                        orderTypeController.deleteAllOrder(codeRecipe);
-                        recipeController.deleteRecipe(codeRecipe);
+                if (confirmation == OK_OPTION) {
+                    ArrayList<Recipe> recipes = recipeController.getAllRecipe();
+                    int[] selectedIndex = this.listSelectionModelRecipe.getSelectedIndices();
+                    for (int index = 0; index < selectedIndex.length; index++) {
+                        int codeRecipe = recipes.get(index).getCode();
+                        this.recipeStepController.deleteAllStepRecipe(codeRecipe);
+                        this.lineRecipeController.deleteAllLineRecip(codeRecipe);
+                        this.equipementController.deleteAllEquipement(codeRecipe);
+                        this.commentController.deleteAllComment(codeRecipe);
+                        this.periodController.deleteAllPeriod(codeRecipe);
+                        this.orderTypeController.deleteAllOrder(codeRecipe);
+                        this.recipeController.deleteRecipe(codeRecipe);
                     }
-                    indexRecipeSelection++;
+                    this.listSelectionModelRecipe.clearSelection();
+                    this.repaint();
+                    this.revalidate();
                 }
-                this.repaint();
-                this.revalidate();// todo : voir c'est quoi la différence entre les deux repaint et revalidate
-                this.listSelectionModelRecipe.clearSelection();//car le truc est à 0 alors et pas à -1 se qu'il fat qu'il continue
-                // à permettre l'élimination même s'il n' a rien à eliminer donc cela deconne
-
-            }catch(DeleteRecipeException exception ){
-               System.out.println("erreur au niveau de l'elimination d'une recipe");
-            } catch(AllRecipeException | DeleteAllEquipementException exception){
-                System.out.println("erreur : getAllRecipe");
-            } catch (DeleteAllOfOneRecipeException | DeleteAllCommentException | DeleteAllLineRecipeException ex) {
-               System.out.println("eliminationRecipePanel : erreur ");
-            }  // todo : faire un truc pour les exceptions, un get message peut être ?
-            catch (DeleteAllPeriodException ex) {
-                ex.printStackTrace();
-            } catch (DeleteAllOrderTypeException ex) {
-                ex.printStackTrace();
+            } catch (Exception exception) {
+                JOptionPane.showMessageDialog(null, "Failed to delete recipe", "Failed To Delete recipe", JOptionPane.ERROR_MESSAGE);
             }
-
-        }
-        else{
-            System.out.println("il faut selectionner pour changer");
         }
     }
 }
