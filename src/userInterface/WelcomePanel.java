072bbde7bd7ab1bc0fc308245023bf0c7d9ccbcc
AppoGit-Hub <>
2023-08-14 23:24:16 +0200

    update welcome page

--- src/userInterface/WelcomePanel.java
+++ src/userInterface/WelcomePanel.java
@@ -4,6 +4,6 @@ import javax.swing.*;
 
 public class WelcomePanel extends JPanel {
     public WelcomePanel() {
-        this.add(new JLabel("Bienvenue dans notre gestionnaire de recettes ! Profitez de 2 mois de travail et de plus de 6500 lignes de code :D"));
+        this.add(new JLabel("Bienvenue dans notre gestionnaire de recettes ! Profitez d'un travail de 5815 lignes de code :D"));
     }
 }
